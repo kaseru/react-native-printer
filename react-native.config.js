08d834dca1b5a5a0b478c24e9c196b9820b3a3f6
DG <kaseru@live.com>
2026-05-06 21:03:54 +0700

    update react-native config

--- react-native.config.js
+++ react-native.config.js
@@ -2,7 +2,7 @@ module.exports = {
   dependency: {
     platforms: {
       android: {
-        packageImportPath: 'import com.xgitvn.printer.PrinterPackage;',
+        packageImportPath: 'import com.xgitvn.printer.core.PrinterPackage;',
         packageInstance: 'new PrinterPackage()',
       },
     },
