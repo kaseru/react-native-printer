965bdf47f3bda5ffd4a8ab5f5a931822d42a4926
DG <kaseru@live.com>
2026-05-07 08:44:26 +0700

    chore: add prettier config and index updates

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,6 @@
-import {
-  EscNetPrinter,
-  EscUsbPrinter,
-  COMMANDS,
-  BluetoothPrinter,
-  EscBluetoothPrinter,
-  TscBluetoothPrinter,
-  TscNetPrinter,
-  TscUsbPrinter,
-} from "./printer";
+import { EscNetPrinter, EscUsbPrinter, COMMANDS, BluetoothPrinter, EscBluetoothPrinter, TscBluetoothPrinter, TscNetPrinter, TscUsbPrinter } from './printer';
 
-export {
-  EscBluetoothPrinter,
-  BluetoothPrinter,
-  TscBluetoothPrinter,
-  COMMANDS,
-  EscNetPrinter,
-  EscUsbPrinter,
-  TscNetPrinter,
-  TscUsbPrinter,
-};
+export { EscBluetoothPrinter, BluetoothPrinter, TscBluetoothPrinter, COMMANDS, EscNetPrinter, EscUsbPrinter, TscNetPrinter, TscUsbPrinter };
 
 export function ping(host: string, timeoutMs: number = 1000) {
   return TscNetPrinter.ping(host, timeoutMs);
