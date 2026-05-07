0b7064a3dce00c822726753e7e8947511a185fa1
DG <kaseru@live.com>
2026-05-07 09:26:22 +0700

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { EscNetPrinter, EscUsbPrinter, COMMANDS, BluetoothPrinter, EscBluetoothPrinter, TscBluetoothPrinter, TscNetPrinter, TscUsbPrinter } from './printer';
+import { EscNetPrinter, EscUsbPrinter, COMMANDS, BluetoothPrinter, EscBluetoothPrinter, TscBluetoothPrinter, TscNetPrinter, TscUsbPrinter, TscCommand } from './printer';
 
-export { EscBluetoothPrinter, BluetoothPrinter, TscBluetoothPrinter, COMMANDS, EscNetPrinter, EscUsbPrinter, TscNetPrinter, TscUsbPrinter };
+export { EscBluetoothPrinter, BluetoothPrinter, TscBluetoothPrinter, COMMANDS, EscNetPrinter, EscUsbPrinter, TscNetPrinter, TscUsbPrinter, TscCommand };
 
 export function ping(host: string, timeoutMs: number = 1000) {
   return TscNetPrinter.ping(host, timeoutMs);
